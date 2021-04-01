5d5942b2c13b1ce7cdb7ec75d52230e8307b8248
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-04-01 17:17:28 +0430

    Add request

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+var axios = require("axios");
+var qs = require("qs");
+var idpay = /** @class */ (function () {
+    function idpay(apiKey, SandBox) {
+        idpay.apiKey = apiKey;
+        idpay.SandBox = SandBox;
+    }
+    idpay.SandBox = false;
+    idpay.urlStart = "https://api.idpay.ir/v1.1/payment";
+    idpay.setConfig = function (url, data) {
+        return {
+            method: "post",
+            url: url,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "X-API-KEY": idpay.apiKey,
+                "X-SANDBOX": idpay.SandBox
+            },
+            data: data
+        };
+    };
+    idpay.setRequest = function (data, url) {
+        var config = idpay.setConfig(url, data);
+        return new Promise(function (resolve, reject) {
+            axios(config)
+                .then(function (response) {
+                resolve(response.data);
+            })["catch"](function (error) {
+                reject(error);
+            });
+        });
+    };
+    idpay.Request = function (para) {
+        var data = qs.stringify(para);
+        return idpay.setRequest(data, "" + idpay.urlStart);
+    };
+    return idpay;
+}());
+module.exports = idpay;
---
+++ src/index.ts
@@ -0,0 +1,66 @@
+var axios = require("axios");
+var qs = require("qs");
+
+type Config = {
+  method: "post" | "get";
+  url: string;
+  headers: {
+    "Content-Type": string;
+    "X-API-KEY": string;
+    "X-SANDBOX": boolean;
+  };
+  data: string;
+};
+
+type RQ = {
+  order_id: string;
+  amount: number;
+  name: string;
+  phone: string;
+  mail: string;
+  desc: string;
+  callback: string;
+};
+
+class idpay {
+  private static apiKey: string;
+  private static SandBox: boolean = false;
+  private static urlStart = "https://api.idpay.ir/v1.1/payment";
+  constructor(apiKey: string, SandBox: boolean) {
+    idpay.apiKey = apiKey;
+    idpay.SandBox = SandBox;
+  }
+
+  private static setConfig = (url: string, data: string): Config => {
+    return {
+      method: "post",
+      url: url,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-API-KEY": idpay.apiKey,
+        "X-SANDBOX": idpay.SandBox,
+      },
+      data: data,
+    };
+  };
+  private static setRequest = (data: string, url: string): Promise<any> => {
+    let config = idpay.setConfig(url, data);
+    return new Promise((resolve, reject) => {
+      axios(config)
+        .then(function (response) {
+          resolve(response.data);
+        })
+        .catch(function (error) {
+          reject(error);
+        });
+    });
+  };
+
+  static Request: Function = (para:RQ): Promise<any> => {
+    const data = qs.stringify(para);
+
+    return idpay.setRequest(data, `${idpay.urlStart}`);
+  };
+}
+
+module.exports = idpay;
